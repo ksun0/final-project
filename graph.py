d62da2c708191b853dcdf093a8003e63aa65353b
Lior Hirschfeld <liortulip@gmail.com>
2017-02-23 18:32:35 -0500

    2 plots

--- graph.py
+++ graph.py
@@ -2,9 +2,17 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn
 import numpy as np
+
 x = list(range(1, 101))
 y = pickle.load(open('pelletsearned.p', 'rb'))[1]
+# plt.figure(1)
+plt.subplot(211)
+plt.scatter(x,y)
+plt.xlabel("Iteration Number")
+plt.ylabel("Pellets Eearned")
 
+# plt.figure(2)
+plt.subplot(212)
 plt.scatter(x,np.log(y))
 plt.xlabel("Iteration Number")
 plt.ylabel("Log of Pellets Eearned")
